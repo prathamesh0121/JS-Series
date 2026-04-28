4d54a5ede6a36d6d7d5d4eff0508f45079ee3759
Prathamesh Tambekar <prathameshtambekar9425@gmail.com>
2026-04-28 10:36:09 +0000

    Variables

---
+++ Js_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 101
+let accountEmail ="pbt123@gamil.com"
+var accountPassword = "12345"
+accountCity = "pune"
+let accountState ;
+
+// const accountId = 102;
+accountEmail ="pratham1234@gmai.com"
+accountPassword="65432"
+accountCity ="vandur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
