6582c0e65f11b167df542660b1a55e240ef63f32
Prathamesh Tambekar <prathameshtambekar9425@gmail.com>
2026-04-29 03:57:43 +0000

    Datatypes

---
+++ Js_basics/Datatypes.js
@@ -0,0 +1,19 @@
+let name ="Prathamesh"
+let age = 23
+let student = true
+let married ;
+let backlogs = null
+let Salary = 1234567890123456789n
+let x = 20
+let y =20
+
+console.table([name,age,student,married,backlogs,Salary,x==y])
+
+console.log(typeof name);
+console.log(typeof student);
+console.log(typeof married);
+console.log(typeof backlogs);
+
+
+
+
--- Js_basics/variables.js
+++ Js_basics/variables.js
@@ -3,10 +3,11 @@ let accountEmail ="pbt123@gamil.com"
 var accountPassword = "12345"
 accountCity = "pune"
 let accountState ;
+var accountcode;
 
 // const accountId = 102;
 accountEmail ="pratham1234@gmai.com"
 accountPassword="65432"
 accountCity ="vandur"
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState,accountcode])
