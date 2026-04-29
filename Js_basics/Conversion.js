220e0d711af4368b58ff00e58b7c8dfc2412160c
Prathamesh Tambekar <prathameshtambekar9425@gmail.com>
2026-04-29 04:29:52 +0000

    Conversion of datatypes

---
+++ Js_basics/Conversion.js
@@ -0,0 +1,107 @@
+// Number 
+
+let age ="23abc"
+console.log(typeof (age));
+let num = Number(age)
+console.log(typeof num);
+console.log(num);
+
+console.log("-------------------------------------");
+
+let score =null
+console.log(typeof (score));
+let num1 = Number(score)
+console.log(typeof num1);
+console.log(num1);
+
+console.log("-----------------------------------------");
+
+let score1 =undefined
+console.log(typeof (score1));
+let num2 = Number(score1)
+console.log(typeof num2);
+console.log(num2);
+
+console.log("-----------------------------------------");
+let score2 =true
+console.log(typeof (score2));
+let num3 = Number(score2)
+console.log(typeof num3);
+console.log(num3);
+
+console.log("-----------------------------------------");
+
+let name ="Prathamesh"
+console.log(typeof (name));
+let num4 = Number(name)
+console.log(typeof num4);
+console.log(num4);
+
+console.log("-----------------------------------------");
+
+
+// Boolean
+let marks =1
+console.log(typeof (marks));
+let val = Boolean(marks)
+console.log(typeof val);
+console.log(val);
+
+console.log("-----------------------------------------");
+
+let marks1 =0
+console.log(typeof (marks1));
+let val1 = Boolean(marks1)
+console.log(typeof val1);
+console.log(val1);
+
+console.log("-----------------------------------------");
+
+let marks2 =""
+console.log(typeof (marks2));
+let val2 = Boolean(marks2)
+console.log(typeof val2);
+console.log(val2);
+
+console.log("-----------------------------------------");
+
+let marks3 ="prathamesh"
+console.log(typeof (marks3));
+let val3 = Boolean(marks3)
+console.log(typeof val3);
+console.log(val3);
+
+console.log("-----------------------------------------");
+
+// String 
+let books =33
+console.log(typeof (books));
+let pen = String(books)
+console.log(typeof pen);
+console.log(pen);
+
+console.log("-----------------------------------------");
+
+let books1 =null
+console.log(typeof (books1));
+let pen1 = String(books1)
+console.log(typeof pen1);
+console.log(pen1);
+
+console.log("-----------------------------------------");
+
+let books2 =undefined
+console.log(typeof (books2));
+let pen2 = String(books2)
+console.log(typeof pen2);
+console.log(pen2);
+
+console.log("-----------------------------------------");
+
+let books3 =true
+console.log(typeof (books3));
+let pen3 = String(books3)
+console.log(typeof pen3);
+console.log(pen3);
+
+console.log("-----------------------------------------");
