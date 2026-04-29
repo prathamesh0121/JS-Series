dd667d2b7b3c68ca5afeab607e83c4f0daac4f40
Prathamesh Tambekar <prathameshtambekar9425@gmail.com>
2026-04-29 10:24:18 +0000

    Arithimatic and String concatination operations

---
+++ Js_basics/Operations.js
@@ -0,0 +1,31 @@
+// Arithematic Operations
+
+console.log(10+5);
+console.log(10-5);
+console.log(10*5);
+console.log(10**2);
+console.log(10/5);
+console.log(10%3);
+console.log("****************************************************");
+
+console.log("10"+2);      //102
+console.log("10"-2);      //8
+console.log("10"*2);      //20
+console.log("10"/2);      //5
+
+
+console.log(10+"2");     //102
+console.log(10-"2");     //8
+console.log(10*"2");     //20
+console.log(10/"2");     //5
+console.log("****************************************************");
+
+console.log("3"+5*6/2);
+
+console.log("****************************************************");
+let str1= "Hello"
+let str2 =" Prathamesh"
+let str3 = str1+str2
+console.log(str3);
+
+
