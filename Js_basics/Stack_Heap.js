7861b919d203f98cb18329fab9692666edca8c8e
Prathamesh Tambekar <prathameshtambekar9425@gmail.com>
2026-05-01 07:13:51 +0000

    Memory allocations

---
+++ Js_basics/Stack_Heap.js
@@ -0,0 +1,18 @@
+let name = "Prathamesh"
+let pname = name
+pname = "tambekar"
+
+console.log(name);
+console.log(pname);
+
+let obj1 ={
+    name : "object one",
+    roll : 101
+}
+
+let obj2 = obj1
+
+obj2.name="object two"
+
+console.log(obj1);
+console.log(obj2);
